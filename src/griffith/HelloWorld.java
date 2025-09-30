f00459837ffc8d5afb60179416870aa22f5343ba
arkady <arkadijrivior@gmail.com>
2025-09-30 13:28:22 +0100

    firstCommit

---
+++ src/griffith/HelloWorld.java
@@ -0,0 +1,37 @@
+package griffith;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		System.out.println("first");
+		
+		//USEFUL COMMANDS
+		/*
+		 CMD-X CUT
+		 CMD-Y REDO
+		 CMD-SHIFT-P MATCHING BRACKETS
+		 CMD-SHIFT-L ALL COMMANDS
+		 sysout-TAB System.out.println()
+		 */
+		
+		//DATA TYPES
+		boolean isStudent = true; //1 bit
+		byte number1 = 127; //8 bits -127 to 127
+		short number2 = 32014; //16 bits -32768 to 32767
+		char symbolLetter = 'A'; //16 bits Letter
+		char symbolNumber = '5'; //Number
+		char symbolUnicode = 75; //Unicode
+		int numberDefault = 1232132133; //32 bits
+		long numberLong = 1394834333333333333l; //64 bits
+		float numberFloat = 4.555f; // 32 bits; f is required
+		double numberDefaultDouble = 34.444444; //64 bits 
+		System.out.println(symbolNumber);
+		System.out.println(symbolUnicode);
+		
+		
+
+	}
+
+}

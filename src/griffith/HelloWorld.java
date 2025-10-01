b4abd14d092fe1d637a0664134162e12d187e030
arkady <arkadijrivior@gmail.com>
2025-10-02 00:33:59 +0100

    changes

--- src/griffith/HelloWorld.java
+++ src/griffith/HelloWorld.java
@@ -17,18 +17,38 @@ public class HelloWorld {
 		 */
 		
 		//DATA TYPES
+		boolean isWednesday = false; //0 bit
 		boolean isStudent = true; //1 bit
-		byte number1 = 127; //8 bits -127 to 127
-		short number2 = 32014; //16 bits -32768 to 32767
-		char symbolLetter = 'A'; //16 bits Letter
-		char symbolNumber = '5'; //Number
-		char symbolUnicode = 75; //Unicode
+		byte smallest = 127; //8 bits -127 to 127
+		short small = 32014; //16 bits -32768 to 32767
 		int numberDefault = 1232132133; //32 bits
 		long numberLong = 1394834333333333333l; //64 bits
 		float numberFloat = 4.555f; // 32 bits; f is required
 		double numberDefaultDouble = 34.444444; //64 bits 
+		char symbolLetter = 'A'; //16 bits Letter
+		char symbolNumber = '5'; //Number
+		char symbolUnicode = 75; //Unicode
 		System.out.println(symbolNumber);
 		System.out.println(symbolUnicode);
+		
+		//DECLARATION
+		byte num; 
+		num = 3; // assignment 
+		char digit = '0';
+		int score, players, points; //1 line declaration 
+		score = 5;
+		players = 6;
+		points = 8;
+		int num1 = 5, num3 = 5; //1 line declaration + assignment 
+		int num22 = (int) 5.5;
+		System.out.println((double)num22); //converting from integer to double
+		int first = 20;
+		int second = 7;
+		System.out.println(first%second);//remaining number
+		System.out.println((10 +5)/(3*7));
+		
+		
+		
 		
 		
 
